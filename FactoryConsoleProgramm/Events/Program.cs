1aad83b7b4aaad2c7ff2935fdcbc1f24ce7b5f27
Валерий <145742155+TesloPlay@users.noreply.github.com>
2024-05-22 23:17:30 +0700

    Events Added

--- FactoryConsoleProgramm/Events/Program.cs
+++ FactoryConsoleProgramm/Events/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Events
+﻿using System.Xml;
+
+namespace Events
 {
     internal class Program
     {
+        public delegate void ReadingDelegate(int arg);
+        static public event ReadingDelegate OnRead;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            UserEnteredNumberEventArgs UENEA = new UserEnteredNumberEventArgs();
+            OnRead += UENEA.WhenAndWhatWasReaded;
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (int.TryParse(line, out int num)) UENEA.WhenAndWhatWasReaded(num);
+            }
+        }
+    }
+
+    internal class UserEnteredNumberEventArgs
+    {
+        public int Input { get; set; }
+        public DateTime EnteredAt { get; set; }
+        
+        public void WhenAndWhatWasReaded(int number)
+        {
+            Input = number;
+            EnteredAt = DateTime.Now;
+            Console.WriteLine($"Пользователь ввёл <{Input}> в <{EnteredAt}>");
         }
     }
 }
